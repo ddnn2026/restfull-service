0576e3818848124657c3cbcfd3310447d63994d6
David Nguyen <ddnn2026@gmail.com>
2023-12-05 16:05:17 +0700

    all user, find user

---
+++ src/main/java/com/example/resfulwebservices/helloWorld/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.example.resfulwebservices.helloWorld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "Hello david";
+    }
+
+    @GetMapping("/hello-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello world bean");
+    }
+
+    @GetMapping("/hello-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBeanPath(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+}
